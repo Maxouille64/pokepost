a0a4ca062ee2ff85dc91b49fb91c9308f1f1d407
Maxouille <xd.maxouille@gmail.com>
2024-09-08 17:00:36 +0200

    Update server.py

--- server.py
+++ server.py
@@ -180,4 +180,4 @@ def result():
     return render_template("index2.html", art = art_data, title = "CSV")
     
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True, port=5000, host="0.0.0.0")
